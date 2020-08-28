289a742413ee4004e69e11cfbee94881c726a6c5
missukevitch <missukevitch@gmail.com>
2020-08-28 15:56:07 +0300

    feat: add birds info

--- songbird/src/data.js
+++ songbird/src/data.js
@@ -106,7 +106,7 @@ export const birdsData = [
       id: 1,
       name: 'Алет',
       species: 'Uria aalge',
-      description: 'Обитает алет около морей, но морской пищей не питается. Гнездится в летний период, что также необычно для пернатых. Мигрирует на очень далекие расстояния – на зиму алет перелетает из родного Средиземноморья на Мадагаскар, чтобы затем вернуться на свои гнездовья.',
+      description: 'Обитает алет около морей, но морской пищей не питается. Мигрирует на очень далекие расстояния – на зиму алет перелетает из родного Средиземноморья на Мадагаскар, чтобы затем вернуться на свои гнездовья.',
       image: 'https://live.staticflickr.com/65535/50152168812_6d44cd8027.jpg%22%20width_m:%20500',
       audio: 'https://raw.githubusercontent.com/missdasha/songbird-data/master/uria_aalge.mp3'
     },
@@ -151,56 +151,6 @@ export const birdsData = [
       audio: 'https://raw.githubusercontent.com/missdasha/songbird-data/master/pernis_apivorus.mp3'
     }
   ],
-  // [
-  //   {
-  //     id: 1,
-  //     name: 'Зяблик',
-  //     species: 'Fringilla coelebs',
-  //     description: 'В дикой природе насчитывается 450 видов зябликов. Зимой зяблики ведут стайный образ жизни. Иногда в их семьях можно увидеть воробьев. Запевают зяблики весной, с наступлением брачного периода. Их пение – это заливистые многоминутные рулады.',
-  //     image: 'https://live.staticflickr.com/65535/49143150817_2d3a2f6c1e.jpg',
-  //     audio: 'https://www.xeno-canto.org/sounds/uploaded/ZNCDXTUOFL/XC512407-150622_03%20zi%C4%99ba%20%282%29.mp3'
-  //   },
-  //   {
-  //     id: 2,
-  //     name: 'Клёст',
-  //     species: 'Loxia curvirostra',
-  //     description: 'Клестов называют «рождественскими» птицами. В естественных условиях они дают потомство зимой – в январе. Эти птицы утепляют свои гнезда мхом и шерстью животных, потому птенцам не холодно. В поисках шишек клесты могут улетать за 3500 км от гнезда.',
-  //     image: 'https://live.staticflickr.com//65535//49365470123_f2de66bb35.jpg',
-  //     audio: 'https://www.xeno-canto.org/sounds/uploaded/OTVUCEGYZN/XC495381-Kruisbek%20roep%20NHD%20290619.mp3'
-  //   },
-  //   {
-  //     id: 3,
-  //     name: 'Горлица',
-  //     species: 'Streptopelia turtur',
-  //     description: 'Горлица обитает в смешанных и широколиственных лесах, а также в городских парках и поселках. Птицы часто выбирают места жизни рядом с человеком и легко привыкают к людям. Благодаря мелодичному приятному пению горлиц часто разводят в домашних условиях.',
-  //     image: 'https://live.staticflickr.com/65535/48063004977_84252f9ceb.jpg',
-  //     audio: 'https://www.xeno-canto.org/sounds/uploaded/ZNCDXTUOFL/XC324106-Turkawka_Streptopelia_turtur_Poland_Jarek_Matusiak_2011625_07.mp3'
-  //   },
-  //   {
-  //     id: 4,
-  //     name: 'Дятел',
-  //     species: 'Dendrocopos major',
-  //     description: 'Дятел – заметная и шумная птица, часто живет рядом с человеком. С середины января до конца июня можно услышать «барабанную дробь» дятлов – трель от вибрации веток под быстрыми ударами клюва птицы. В хорошую погоду дробь слышна в радиусе 1,5 км.',
-  //     image: 'https://live.staticflickr.com/65535/49339376578_e933426455.jpg',
-  //     audio: 'https://www.xeno-canto.org/sounds/uploaded/ZNCDXTUOFL/XC518928-AB-017%20dzi%C4%99cio%C5%82%20du%C5%BCy%20agresja%20%282%29.mp3'
-  //   },
-  //   {
-  //     id: 5,
-  //     name: 'Удод',
-  //     species: 'Upupa epops',
-  //     description: 'Удоды предпочитают жить на открытых ландшафтах с отдельными деревьями или рощами. Наиболее удобными для птицы являются лесостепь и саванна. Удод может выбирать места жительства рядом с человеком: пастбища, виноградники, фруктовые сады.',
-  //     image: 'https://live.staticflickr.com//65535//49226383598_6f8be86a06.jpg',
-  //     audio: 'https://www.xeno-canto.org/sounds/uploaded/ZNCDXTUOFL/XC477326-dudek%20%282%29.mp3'
-  //   },
-  //   {
-  //     id: 6,
-  //     name: 'Стриж',
-  //     species: 'Apus apus',
-  //     description: 'Стрижа можно увидеть практически в каждом уголке планеты. Они обитают как в лесных зонах, так и на открытых местностях. Живут стрижи крупными стаями. Большие колонии этих птиц можно увидеть в городах или на прибрежных скалах.',
-  //     image: 'https://live.staticflickr.com//65535//48386150031_7b749df74b.jpg',
-  //     audio: 'https://www.xeno-canto.org/sounds/uploaded/TMUAWSDHDJ/XC511871-G.mp3'
-  //   }
-  // ],
   [
     {
       id: 1,
@@ -304,51 +254,51 @@ export const birdsData = [
   [
     {
       id: 1,
-      name: 'Альбатрос',
-      species: 'Diomedea exulans',
-      description: 'Альбатрос - самая крупная летающая птица в мире. Размах крыльев достигает три с половиной, вес - десять килограммов. Большую часть жизни альбатросы проводят в воздухе, покрывая над океанскими просторами огромные расстояния',
-      image: 'https://live.staticflickr.com/7557/16260253965_8e9430cb66.jpg',
-      audio: 'https://www.xeno-canto.org/sounds/uploaded/WOEAFQRMUD/XC293087-Diomedea%20exulans151120_T254.mp3'
+      name: 'Фрегат',
+      species: 'Fregata',
+      description: 'У всех фрегатов крылья узкие, а хвост длинный и раздвоенный, напоминающий английскую букву W. Такие приспособления необходимы птице для маневренности в полете. Летают фрегаты очень искусно. ',
+      image: 'https://live.staticflickr.com/65535/49721391028_273d147d08.jpg',
+      audio: 'https://raw.githubusercontent.com/missdasha/songbird-data/master/fregata.mp3'
     },
     {
       id: 2,
-      name: 'Олуша',
-      species: 'Sula nebouxii',
-      description: 'Особенностью голубоногой олуши является не только насыщенный ярко-синий цвет лапок, но еще и тот факт, что они очень теплые. В то время как другие виды птиц греют кладки своим телом, голубоногая олуша делает это с помощью лапок',
-      image: 'https://live.staticflickr.com/800/40645471394_4422e69ed8.jpg',
-      audio: 'https://www.xeno-canto.org/sounds/uploaded/YHKQPPJDVP/XC411507-171217_1491%20BF%20Booby%205ft%20IDLP%201230%20mp3%20amp.mp3'
+      name: 'Тупик',
+      species: 'Fratercula arctica',
+      description: 'Тупики быстро ходят по земле и даже могут бежать, но делают это вразвалку. Птицы отлично приспособлены к плаванию и нырянию, дыхание под водой задерживают на минуту. ',
+      image: 'https://live.staticflickr.com/65535/50277102498_f91fc7d358.jpg',
+      audio: 'https://raw.githubusercontent.com/missdasha/songbird-data/master/fratercula_arctica.mp3'
     },    
     {
       id: 3,
-      name: 'Буревестник',
-      species: 'Puffinus griseus',
-      description: 'Размеры буревестниковых разные. Самые маленькие из них в длину составляют до 25 см, самые большие - до 1 м, при размахе крыльев около 2 м. Существует поверье, что появление буревестника в воздухе предвещает бурю, о чем говорит само название птицы.',
-      image: 'https://live.staticflickr.com//607//22136056020_935cb113f9.jpg',
-      audio: 'https://www.xeno-canto.org/sounds/uploaded/XQEVNREHJY/SHEARWATER%20Christmas%20Island_04_Motu_Isla%20de%20Pascua-Easter%20Island_CH_4MAR03_Alvaro%20Jaramillo.mp3'
+      name: 'Поморник',
+      species: 'Stercorarius',
+      description: 'Когда поморник замечает опасность, он оповещает своих сородичей короткими и низкими звуками, а вот в случае собственного нападения на других птиц, наоборот, издает громкие вибрирующие звуки.',
+      image: 'https://live.staticflickr.com/65535/50274538676_2815934616.jpg',
+      audio: 'https://raw.githubusercontent.com/missdasha/songbird-data/master/stercorarius_chilensis.mp3'
     },
     {
       id: 4,
-      name: 'Пеликан',
-      species: 'Pelecanus',
-      description: 'Пеликаны — обитатели морей и рек. Ходят они неуклюже, но хорошо летают и плавают. Питаются в основном рыбой, устраивают коллективные охоты — выстроившись полукругом хлопают по воде крыльями и клювами и вытесняют напуганную рыбу на мелководье.',
-      image: 'https://live.staticflickr.com/7885/46523771945_9496c2a191.jpg',
-      audio: 'https://www.xeno-canto.org/sounds/uploaded/XAMHIHFTZG/XC331138-call1.mp3'
+      name: 'Глупыш',
+      species: 'Fulmarus glacialis',
+      description: 'Глупыши – долгожители среди птиц, их продолжительность жизни достигает 40-50 лет. В Шотландии существуют заказники, где глупыши включены в списки охраняемых видов.',
+      image: 'https://live.staticflickr.com/65535/48919388072_fd26219a98.jpg',
+      audio: 'https://raw.githubusercontent.com/missdasha/songbird-data/master/fulmarus_glacialis.mp3'
     },
     {
       id: 5,
-      name: 'Пингвин',
-      species: 'Aptenodytes forsteri',
-      description: 'Самец императорского пингвина достигает роста 130 см, его масса может приближаться к 50 кг. Из всех современных пингвинов этот вид – самый крупный. Питание пингвина состоит из рыбы, кальмаров и криля. Охотятся птицы в океане большими группами.',
-      image: 'https://live.staticflickr.com/5202/5252413926_8e013a3efd.jpg',
-      audio: 'https://www.xeno-canto.org/sounds/uploaded/OOECIWCSWV/XC449827-LS100829%20King%20Penguin%20call%20A.mp3'
+      name: 'Галстучник',
+      species: 'Charadrius hiaticula',
+      description: 'Для того, чтобы отвлечь непрошеных гостей, которые появляются у гнезда, галстучник притворяется раненым.',
+      image: 'https://live.staticflickr.com/65535/48919616573_afde2630a2.jpg',
+      audio: 'https://raw.githubusercontent.com/missdasha/songbird-data/master/charadrius_hiaticula.mp3'
     },
     {
       id: 6,
-      name: 'Чайка',
-      species: 'Larus argentatus',
-      description: 'Чайки населяют берега морей, озёр, рек, водохранилищ, болот, часто гнездятся на островах. С конца прошлого века чайки стали появляться в крупных городах, где устраивает гнёзда на крышах зданий. Все чайки ведут колониальный образ жизни.',
-      image: 'https://live.staticflickr.com/65535/48577115317_7034201dde.jpg',
-      audio: 'https://www.xeno-canto.org/sounds/uploaded/VOLIQOYWKG/XC501190-190801_06.50h_zilvermeeuw_duinen%20van%20goeree_roep_2ex_overvliegend_gezien_.mp3'
+      name: 'Гагарка',
+      species: 'Alca torda',
+      description: 'Отнять у гагарки из клюва пойманную рыбу практически невозможно, птица будет ее защищать очень агрессивно. В то же время сами гагарки часто нападают на других птиц, чтобы отнять у них корм.',
+      image: 'https://live.staticflickr.com/860/43589081972_41834af76d.jpg',
+      audio: 'https://raw.githubusercontent.com/missdasha/songbird-data/master/alca_torda.mp3'
     }
   ]
 ];
